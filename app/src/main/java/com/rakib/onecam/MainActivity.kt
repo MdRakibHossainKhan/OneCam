2e08391be0036d5236a319b6e57319e5e2669587
Md. Rakib Hossain Khan <md.rakib.hossain.khan@gmail.com>
2023-02-14 17:52:01 +0600

    Image Capture Feature Added

--- app/src/main/java/com/rakib/onecam/MainActivity.kt
+++ app/src/main/java/com/rakib/onecam/MainActivity.kt
@@ -1,11 +1,277 @@
 package com.rakib.onecam
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Log
+import android.view.KeyEvent
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.camera.core.*
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.view.PreviewView
+import androidx.core.content.ContextCompat
+import androidx.databinding.DataBindingUtil
+import com.google.common.util.concurrent.ListenableFuture
+import com.rakib.onecam.databinding.ActivityMainBinding
+import java.io.File
+import java.nio.ByteBuffer
+import java.text.SimpleDateFormat
+import java.util.*
+import java.util.concurrent.Executors
+import java.util.concurrent.TimeUnit
+
+private const val REQUEST_CODE_PERMISSIONS = 10
+
+// An array of all the permissions specified in the manifest
+private val REQUIRED_PERMISSIONS =
+    arrayOf(android.Manifest.permission.CAMERA, android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var cameraProviderFuture: ListenableFuture<ProcessCameraProvider>
+    private lateinit var imagePreview: Preview
+    private lateinit var previewView: PreviewView
+    private lateinit var imageAnalysis: ImageAnalysis
+    private lateinit var imageCapture: ImageCapture
+    private val executor = Executors.newSingleThreadExecutor()
+    private lateinit var outputDirectory: File
+    private lateinit var cameraControl: CameraControl
+    private lateinit var cameraInfo: CameraInfo
+    private var linearZoom = 0f
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.lifecycleOwner = this
+
+        previewView = binding.previewView
+
+        cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+
+        // Request camera permissions
+        if (allPermissionsGranted()) {
+            previewView.post { startCamera() }
+        } else {
+            requestPermissions(
+                REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS
+            )
+        }
+
+        outputDirectory = getOutputDirectory(this)
+
+        binding.cameraCaptureButton.setOnClickListener {
+            takePicture()
+        }
+
+        binding.cameraTorchButton.setOnClickListener {
+            toggleTorch()
+        }
+    }
+
+    private fun toggleTorch() {
+        if (cameraInfo.torchState.value == TorchState.ON) {
+            cameraControl.enableTorch(false)
+        } else {
+            cameraControl.enableTorch(true)
+        }
+    }
+
+    private fun takePicture() {
+        val file = createFile(
+            outputDirectory, FILENAME, PHOTO_EXTENSION
+        )
+
+        val outputFileOptions = ImageCapture.OutputFileOptions.Builder(file).build()
+
+        imageCapture.takePicture(outputFileOptions,
+            executor,
+            object : ImageCapture.OnImageSavedCallback {
+                override fun onImageSaved(outputFileResults: ImageCapture.OutputFileResults) {
+                    val msg = "Photo Capture Succeeded: ${file.absolutePath}"
+                    previewView.post {
+                        Toast.makeText(this@MainActivity, msg, Toast.LENGTH_LONG).show()
+                    }
+                }
+
+                override fun onError(exception: ImageCaptureException) {
+                    val msg = "Photo Capture Failed: ${exception.message}"
+                    previewView.post {
+                        Toast.makeText(this@MainActivity, msg, Toast.LENGTH_LONG).show()
+                    }
+                }
+            })
+    }
+
+    private fun startCamera() {
+        // Build the viewfinder use case
+        imagePreview = Preview.Builder().apply {
+            setTargetAspectRatio(AspectRatio.RATIO_16_9)
+            setTargetRotation(previewView.display.rotation)
+        }.build()
+
+        imageAnalysis = ImageAnalysis.Builder().apply {
+            setImageQueueDepth(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+        }.build()
+
+        imageAnalysis.setAnalyzer(executor, LuminosityAnalyzer())
+
+        imageCapture = ImageCapture.Builder().apply {
+            setCaptureMode(ImageCapture.CAPTURE_MODE_MINIMIZE_LATENCY)
+            setFlashMode(ImageCapture.FLASH_MODE_AUTO)
+        }.build()
+
+        val cameraSelector =
+            CameraSelector.Builder().requireLensFacing(CameraSelector.LENS_FACING_BACK).build()
+
+        cameraProviderFuture.addListener({
+            val cameraProvider = cameraProviderFuture.get()
+
+            // The use case is bound to an Android Lifecycle with the following code
+            val camera = cameraProvider.bindToLifecycle(
+                this, cameraSelector, imagePreview, imageAnalysis, imageCapture
+            )
+
+            // PreviewView creates a surface provider and is the recommended provider
+            imagePreview.setSurfaceProvider(previewView.surfaceProvider)
+
+            cameraControl = camera.cameraControl
+            cameraInfo = camera.cameraInfo
+            setTorchStateObserver()
+            setZoomStateObserver()
+        }, ContextCompat.getMainExecutor(this))
+    }
+
+    private fun setTorchStateObserver() {
+        cameraInfo.torchState.observe(this) { state ->
+            if (state == TorchState.ON) {
+                binding.cameraTorchButton.setImageDrawable(
+                    ContextCompat.getDrawable(
+                        this, R.drawable.ic_flash_on_24dp
+                    )
+                )
+            } else {
+                binding.cameraTorchButton.setImageDrawable(
+                    ContextCompat.getDrawable(
+                        this, R.drawable.ic_flash_off_24dp
+                    )
+                )
+            }
+        }
+    }
+
+    private fun setZoomStateObserver() {
+        cameraInfo.zoomState.observe(this) { state ->
+            state.linearZoom
+            state.zoomRatio
+            state.maxZoomRatio
+            state.minZoomRatio
+            Log.d(TAG, "${state.linearZoom}")
+        }
+    }
+
+    /**
+     * Process result from permission request dialog box, has the request
+     * been granted? If yes, start Camera. Otherwise display a toast
+     */
+    override fun onRequestPermissionsResult(
+        requestCode: Int, permissions: Array<out String>, grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            if (allPermissionsGranted()) {
+                previewView.post { startCamera() }
+            } else {
+                Toast.makeText(
+                    this, "Permissions not granted by the user.", Toast.LENGTH_SHORT
+                ).show()
+                finish()
+            }
+        }
+    }
+
+    /**
+     * Check if all permission specified in the manifest have been granted
+     */
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(this, it) == PackageManager.PERMISSION_GRANTED
+    }
+
+    // Manage camera Zoom
+    override fun onKeyDown(keyCode: Int, event: KeyEvent): Boolean {
+        return when (keyCode) {
+            KeyEvent.KEYCODE_VOLUME_UP -> {
+                if (linearZoom <= 0.9) {
+                    linearZoom += 0.1f
+                }
+                cameraControl.setLinearZoom(linearZoom)
+                true
+            }
+            KeyEvent.KEYCODE_VOLUME_DOWN -> {
+                if (linearZoom >= 0.1) {
+                    linearZoom -= 0.1f
+                }
+                cameraControl.setLinearZoom(linearZoom)
+                true
+            }
+            else -> super.onKeyDown(keyCode, event)
+        }
+    }
+
+    private class LuminosityAnalyzer : ImageAnalysis.Analyzer {
+        private var lastAnalyzedTimestamp = 0L
+
+        /**
+         * Helper extension function used to extract a byte array from an
+         * image plane buffer
+         */
+        private fun ByteBuffer.toByteArray(): ByteArray {
+            rewind()    // Rewind the buffer to zero
+            val data = ByteArray(remaining())
+            get(data)   // Copy the buffer into a byte array
+            return data // Return the byte array
+        }
+
+        override fun analyze(image: ImageProxy) {
+            val currentTimestamp = System.currentTimeMillis()
+            // Calculate the average luma no more often than every second
+            if (currentTimestamp - lastAnalyzedTimestamp >= TimeUnit.SECONDS.toMillis(1)) {
+                // Since format in ImageAnalysis is YUV, image.planes[0]
+                // contains the Y (luminance) plane
+                val buffer = image.planes[0].buffer
+                // Extract image data from callback object
+                val data = buffer.toByteArray()
+                // Convert the data into an array of pixel values
+                val pixels = data.map { it.toInt() and 0xFF }
+                // Compute average luminance for the image
+                val luma = pixels.average()
+                // Log the new luma value
+                Log.d("CameraXApp", "Average luminosity: $luma")
+                // Update timestamp of last analyzed frame
+                lastAnalyzedTimestamp = currentTimestamp
+            }
+            image.close()
+        }
+    }
+
+    companion object {
+        private const val TAG = "MainActivity"
+        private const val FILENAME = "yyyy-MM-dd-HH-mm-ss-SSS"
+        private const val PHOTO_EXTENSION = ".jpg"
+
+        fun getOutputDirectory(context: Context): File {
+            val appContext = context.applicationContext
+            val mediaDir = context.externalMediaDirs.firstOrNull()?.let {
+                File(it, appContext.resources.getString(R.string.app_name)).apply { mkdirs() }
+            }
+
+            return if (mediaDir != null && mediaDir.exists()) mediaDir else appContext.filesDir
+        }
+
+        fun createFile(baseFolder: File, format: String, extension: String) = File(
+            baseFolder,
+            SimpleDateFormat(format, Locale.US).format(System.currentTimeMillis()) + extension
+        )
     }
 }
